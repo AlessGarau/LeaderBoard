f809f507af08a4fa73729627c81f0b17b29efea7
Aless <117027513+AlessGarau@users.noreply.github.com>
2023-02-16 17:22:35 +0100

    Add files via upload

--- leaderboard.js
+++ leaderboard.js
@@ -1,70 +1,73 @@
-// ============ CLASS PLAYER ============
-class Player {
-    constructor(name, score, score2){
+// ============ CLASS Stage ============
+
+class Stage {
+    constructor(name){
         this.name = name
-        this.score = score
-        this.score2 = score2
-    }
-}
-class Map {
-    constructor(name, player){
-        this.name = name
-        this.player = []
+        this.leaderboard = new Map()
 
     }
+
+    addPlayer(name, score){
+        this.leaderboard.set(name, score)
+    }
 }
 
-let p1 = new Player("Louisan", 25000, 2000)
-let p2 = new Player("Benjos", 10000, 3000)
-let p3 = new Player("Djédjé", 9000, 1000)
-let p4 = new Player("Julien", 13000, 7000)
-let p5 = new Player("Alexis", 7000, 200)
 
-let map1 = new Map("MAP 1")
-let map2 = new Map("MAP 2")
-let map3 = new Map("MAP 3")
-let map4 = new Map("MAP 4")
-let map5 = new Map("MAP 5")
+// ============ CREATE STAGE ============
 
-playerList = [p1, p2, p3, p4, p5]
-mapList = [map1, map2, map3, map4, map5]
+let stage1 = new Stage("stage 1")
+stage1.addPlayer("Louisan", 10000),
+stage1.addPlayer("Benjos", 200),
+stage1.addPlayer("Djédjé", 9000),
+stage1.addPlayer("Julien", 13000),
+stage1.addPlayer("Alexis", 7000)
 
-// ============ AFFICHER BOUTON MAP ============
 
-let i = 1000
-for(map of mapList){
-    let id = i
-    id = String(id)
-    button = document.createElement("button")
-    button.id = id 
-    button.innerText = map.name
-    divBouton = document.querySelector("div.button")
-    divBouton.append(button)
+let stage2 = new Stage("stage 2")
+stage2.addPlayer("Louisan", 788),
+stage2.addPlayer("Benjos", 07654),
+stage2.addPlayer("Djédjé", 4579),
+stage2.addPlayer("Julien", 2649),
+stage2.addPlayer("Alexis", 222222)
+
+let stage3 = new Stage("stage 3")
+stage3.addPlayer("Louisan", 8678),
+stage3.addPlayer("Benjos", 647),
+stage3.addPlayer("Djédjé", 2974),
+stage3.addPlayer("Julien", 1334),
+stage3.addPlayer("Alexis", 11111)
+
+let stage4 = new Stage("stage 4")
+stage4.addPlayer("Louisan", 4522),
+stage4.addPlayer("Benjos", 23333),
+stage4.addPlayer("Djédjé", 97000),
+stage4.addPlayer("Julien", 87655),
+stage4.addPlayer("Alexis", 00000)
+
+// let map3 = new Stage("MAP 3")
+// let map4 = new Stage("MAP 4")
+// let map5 = new Stage("MAP 5")
+
+
+
+// ============ PRINT SCOREBOARD ============
+
+function scoreboard2(stage){
+    // Map to array
+    let stageArray = Array.from(stage.leaderboard)
     
-    let btn = document.getElementById(id)
-    btn.addEventListener('click', () =>{
-    btn.classList.toggle("background")
-    btn.classList.toggle("textColor")
-    resetContain(contain)    
-    scoreboard()
-})
-    id = parseInt(id)
-    i--
-}
+    // Sort player most point to less
+    stageArray.sort((a, b) => {
+    return a[1] - b[1]
+    })
+    stageArray.reverse()
 
-// ============ AFFICHER JOUEUR SCOREBOARD ============
-
-//container classement
-let contain = document.createElement("div")
-contain.className = "contain"
-
-
-// Afficher les joueurs dans le scoreboard
-function scoreboard(){
-    let listSize = playerList.length
+    // variable for iteration
+    let listSize = stageArray.length
     let i = 0
     let place = 1
     let id = i
+
     while(i < listSize){
         // conversion string pour id 
         id = String(id)
@@ -85,19 +88,16 @@ function scoreboard(){
         let spanName = document.createElement("span")
         spanName.className = "joueur"
         spanName.classList.add("nom")
-        spanName.innerText = playerList[i].name
+        spanName.innerText = stageArray[i][0]
 
         // span de son nombre de points
         let spanPoints = document.createElement("span")
         spanPoints.className = "points"
         spanPoints.classList.add("puntos")
-        spanPoints.innerText = playerList[i].score
+        spanPoints.innerText = stageArray[i][1]
 
-    
         // Création de hr et ajout à la page
         let hr = document.createElement("hr")
-        container = document.querySelector("div.container")
-        container.append(contain)
         contain.append(classement)
 
         // Ajout de tous les span dans la div classement
@@ -114,67 +114,63 @@ function scoreboard(){
         place += 1
     }
 }
-
-function scoreboard2(){
-    let listSize = playerList.length
-    let i = 0
-    let place = 1
-    let id = i
-    while(i < listSize){
-        let id = i
-        // conversion string pour id 
-        id = String(id)
-        place = String(place)
-
-
-        // Creation div classement
-        let classement = document.createElement("div")
-        classement.className = "classement"
-        classement.id = id
-
-        // span Numero
-        let spanNumber = document.createElement("span")
-        spanNumber.className = "place"
-        spanNumber.classList.add("numero")
-        spanNumber.innerText = place
-
-        // span nom du joueur
-        let spanName = document.createElement("span")
-        spanName.className = "joueur"
-        spanName.classList.add("nom")
-        spanName.innerText = playerList[i].name
-
-        // span de son nombre de points
-        let spanPoints = document.createElement("span")
-        spanPoints.className = "points"
-        spanPoints.classList.add("puntos")
-        spanPoints.innerText = playerList[i].score2 // a terme avoir une class map et prendre comme arg la map et faire map.score en fonction du player
-
-    
-        // Création de hr et ajout à la page
-        let hr = document.createElement("hr")
-        container = document.querySelector("div.container")
-    
-        container.append(contain)
-        contain.append(classement)
-
-        // Ajout de tous les span dans la div classement
-        let div = document.getElementById(id)
-        div.append(spanNumber)
-        div.append(spanName)
-        div.append(spanPoints)
-        contain.append(hr)
-
-        place = parseInt(place)
-        id = parseInt(id)
-        id += 1
-        i++
-        place += 1
-
-    }
-}
-
 function resetContain(balise){
     balise.innerHTML =""
-    console.log("ici")
+}
+
+// ============ AFFICHER BOUTON MAP AND USE THEM ============
+
+let contain = document.querySelector("div.contain")
+stageList = [stage1, stage2, stage3, stage4]
+let i = 1000
+let btnList = []
+
+for(let stage of stageList){
+
+    let id = i
+    // id to string
+    id = String(id)
+
+    // Create button
+    let btn = document.createElement("button")
+    // Set ID
+    btn.id = id 
+    // Set Text
+    btn.innerText = stage.name
+    // Set class 
+    btn.className = ("btn")
+    // Add button to HTML
+    divBouton = document.querySelector("div.button")
+    divBouton.append(btn)
+
+    let printScoreboard = document.getElementById(id)
+    printScoreboard.addEventListener('click', () =>{
+        // reset content
+        resetContain(contain)    
+        // Print scoreboard
+        scoreboard2(stage)
+    })
+ 
+    // Sélection de tous les boutons
+    const buttons = document.querySelectorAll('.btn');
+
+    // Ajout d'un gestionnaire d'événement à chaque bouton
+    buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Suppression de la classe 'active' pour tous les boutons
+        buttons.forEach(button => {
+        button.classList.remove('background')
+        button.classList.remove('textColor')
+        });
+        // Ajout de la classe 'active' pour le bouton sélectionné
+        button.classList.add('background')
+        button.classList.add('textColor')
+
+        })
+    })
+
+  // ============ AFFICHER JOUEUR SCOREBOARD ============
+
+    id = parseInt(id)
+    i--
 }
